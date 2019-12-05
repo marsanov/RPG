3486bde8f34831cad49a0a0fa61c593e49c99c89
gleb-marsanov <gleb.marsanov@gmail.com>
2019-12-05 18:14:17 +0300

    Добавил префаб стрелы

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+    [SerializeField] private float speed = 1;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsuleCollider = target.GetComponent<CapsuleCollider>();
+        if (targetCapsuleCollider == null)
+        {
+            return target.position;
+        }
+        return target.position + Vector3.up * targetCapsuleCollider.height / 2;
+    }
+}
