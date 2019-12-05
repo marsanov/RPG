e4a23410783cc5577edc91ac271f6ed752320b34
gleb-marsanov <gleb.marsanov@gmail.com>
2019-12-05 14:39:43 +0300

    Добавлена система экипировки одноручного оружия

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using RPG.Core;
 using RPG.Movement;
@@ -12,10 +13,17 @@ namespace RPG.Combat
         [SerializeField] private float weaponRange = 2f;
         [SerializeField] private float timeBetweenAttacks = 1f;
         [SerializeField] private float weaponDamage = 5f;
+        [SerializeField] private GameObject weaponPrefab = null;
+        [SerializeField] private Transform handTransform = null;
 
         private Health target;
         private float timeSinceLastAttack = Mathf.Infinity;
-        
+
+        void Start()
+        {
+            SpawnWeapon();
+        }
+
         void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
@@ -32,6 +40,11 @@ namespace RPG.Combat
                 GetComponent<Mover>().Cancel();
                 AttackBehaviour();
             }
+        }
+
+        private void SpawnWeapon()
+        {
+            Instantiate(weaponPrefab, handTransform);
         }
 
         private void AttackBehaviour()
