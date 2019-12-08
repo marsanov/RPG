9a0df7676e0131de8191f4ca2f6327a9e3bd5f42
gleb-marsanov <gleb.marsanov@gmail.com>
2019-12-09 00:53:58 +0300

    Добавлено сохранение очков опыта

--- Assets/Scripts/Resources/Expirience.cs
+++ Assets/Scripts/Resources/Expirience.cs
@@ -1,14 +1,25 @@
-﻿using UnityEngine;
+﻿using RPG.Saving;
+using UnityEngine;
 
 namespace RPG.Resources
 {
-    public class Expirience : MonoBehaviour
+    public class Expirience : MonoBehaviour, ISaveable
     {
         [SerializeField] private float expiriencePints = 0;
-
+        
         public void GainExpirience(float expirience)
         {
             expiriencePints += expirience;
+        }
+
+        public object CaptureState()
+        {
+            return expiriencePints;
+        }
+
+        public void RestoreState(object state)
+        {
+            expiriencePints = (float) state;
         }
     }
 }
